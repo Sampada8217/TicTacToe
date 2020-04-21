294a2599d97660a31671f758ab0a2c92bcbf8111
sampada8217 <sampadatirthkar@gmail.com>
2020-04-21 13:32:57 +0530

    Print Board After EveryMove

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,24 +1,5 @@
 #!/bin/bash 
 
-function resetBoard()
-{
-echo "Welcome to Tic Tac Toe Game"
-MY_TURN=O
-COMP_TURN=X
-BOARD_SIZE=3
-BOARD_POS=9
-player1=$MY_TURN
-Player2=$COMP_TURN
-
-for (( i=0; i<=$BOARD_POS; i++ ))
-do
-     BOARD[$i]="."
-done
-
-
-}
-
-
 function CheckFirstPlayer()
 {
 	toss=$((RANDOM%2))
@@ -26,18 +7,115 @@ function CheckFirstPlayer()
 	then
 		echo "First turn is MY_TURN"
         else
-                echo "First turn is COMP_TURN"
- 
- 
+		echo "First Turn is COMP_TURN"
+        fi
+}
+function resetBoard()
+{
+echo "Welcome to Tic Tac Toe Game"
+MY_TURN=O
+COMP_TURN=X
+BOARD_SIZE=3
+BOARD_POS=10
+player=$toss
+won=0
+emptySym='.'
+for (( i=1; i<=$BOARD_POS; i++ ))
+do
+     BOARD[$i]="."
+done
+}
+
 function printBoard()
 {
-	echo " | ${Matrix[0]} | ${Matrix[1]} | ${Matrix[2]} | "
+	echo " | ${BOARD[1]} | ${BOARD[2]} | ${BOARD[3]} | "
 	echo "------------"
-	echo " | ${Matrix[3]} | ${Matrix[4]} | ${Matrix[5]} | "
+	echo " | ${BOARD[4]} | ${BOARD[5]} | ${BOARD[6]} | "
 	echo "------------"
-	echo " | ${Matrix[6]} | ${Matrix[7]} | ${Matrix[8]} | "
+	echo " | ${BOARD[7]} | ${BOARD[8]} | ${BOARD[9]} | "
 	echo "------------"
 }
 printBoard
+
+function checkHorizonatal()
+{
+	if [ $won == 0 ]
+        then
+             local rows=3
+             local pos=1
+             local symCount=1
+             while [ $symCount -ne $rows ]
+             do
+                  if [[ ${BOARD[$pos]} == ${BOARD[$pos+1]} ]] && [[ ${BOARD[$pos+1]} == ${BOARD[$pos+2]} ]] && [[ ${BOARD[$pos]} == ${BOARD[$pos+2]} ]] && [[ ${BOARD[$pos]} == $1 ]] 
+		  then
+		  	      printBoard
+                  	      echo "Won Horizonatally"
+                              won=1
+			      break
+		  else
+                              pos=$(( $pos + $rows ))
+		   fi
+
+ 	     symCount=$(( $symCount + 1 ))
+	     done
+          fi
+}
+
+function checkVertical()
+{
+	if [ $won == 0 ]
+        then
+		local col=3
+		local pos=1
+		local symCount=1
+		while [ $symCount -ne $rows ]
+		do
+                	if [[ ${BOARD[$pos]} == ${BOARD[$pos+3]} ]] && [[ ${BOARD[$pos]} == ${BOARD[$pos+6]} ]] && [[ ${BOARD[$pos+3]} == ${BOARD[$pos+6]} ]] && [[ ${BOARD[$pos]} == $1 ]] 
+          		then
+                        	printBoard
+                                echo "Won Vertically"
+                                won=1
+                                break
+                        else
+                               pos=$(( $pos + $col ))
+                        fi
+                symCount=$(( $symCount + 1))
+		done
+         fi
+}
+
+function checkDiagonal()
+{
+	if [ $won == 0 ]
+	then
+		local pos=1
+		if [ ${BOARD[$pos]} == ${BOARD[$pos+4]} ] && [ ${BOARD[$pos+4]} == ${BOARD[$pos+8]} ] && [ ${BOARD[$pos]} == ${BOARD[$pos+8]} ] && [ ${BOARD[$pos]} == $1 ]
+		then
+			printBoard
+			echo "Won Diagonally"
+			won=1
+			break
+                elif [[ ${BOARD[pos+2]} == ${BOARD[pos+4]} ]] && [[ ${BOARD[$pos+2]} == ${BOARD[$pos+6]} ]] && [[ ${BOARD[$pos+4]} == ${BOARD[$pos+6]} ]] && [[ ${BOARD[$pos+2]} == $1 ]] 
+		then
+			printBoard
+			echo "Won Diagonally"
+			won=1
+			break
+		fi
+	fi
+}
+function  Player1()
+{
+	echo "Enter Position where you want to place Symbol"
+	read pos
+
+	if [ ${BOARD[$pos]} == $emptySym ]
+        then
+		BOARD[$pos]=$MY_TURN
+		player=$(( (player%2) +1 ))
+	else
+		echo "You Can't Place There"
+        fi
+}
 
 
